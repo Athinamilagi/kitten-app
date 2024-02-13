e89335203e4065fa2bebaa970927e36774915cbe
Athinamilagi <kvigneshvicky6794@gmail.com>
2024-02-14 00:40:51 +0530

    packages added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Game from "./components/Game";
+import LeaderBoard from "./components/LeaderBoard";
 function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
